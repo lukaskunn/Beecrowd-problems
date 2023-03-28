a8f7f5d01d945d464e40029f7bfe9a1426a3e64e
lukaskunn <lucassioliveira098@gmail.com>
2023-03-28 20:38:56 -0300

    created project template

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+//Uncoment the next line with problem number to import the class tith the solve function
+//#include "./include/Problem_2374.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    //Uncoment the next lines to run the code, dont forget to use the right issue number in the class name
+    //Problem_2374 issue;
+    //issue.resolve();
+
+    system("pause");
     return 0;
 }
